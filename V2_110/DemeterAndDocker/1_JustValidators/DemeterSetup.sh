26876a6e2bc6c19b0a7ceec399ccb277b4c3d08c
tony_livingston@live.com.au <tony_livingston@live.com.au>
2024-02-27 11:36:57 +0000

    setup update

---
+++ V2_110/DemeterAndDocker/1_JustValidators/DemeterSetup.sh
@@ -0,0 +1,33 @@
+https://demeter.run
+
+login using github or create an account
+
+start with a large workspace but convert  to small after setup
+
+choose plutus pioneer 4 from the packages  -- used plutus hello world v2 initially for early learning
+
+open visual studio after it has provisioned
+
+fork the below git repositories to your github then clone them in the terminal
+
+repo$ git clone https://github.com/fromyourforkedrepositories/EmurgoAcademyPlutusExamples.git
+
+repo$ git clone https://github.com/fromyourforkedrepositories/Plutus-V2-Starter-Kit.git
+
+DEMETER SETUP
+
+repo$ cd EmurgoAcademyPlutusExamples
+repo$ cd V2_110
+repo$ cd DemeterAndDocker
+repo$ cd 1_JustValidators/
+repo$ cabal update
+repo$ cabal repl
+    $ :q
+    $ exit
+
+
+
+
+change workspace to small to conserve resources
+
+make sure you pause your workspace and log out of node when finished to conserve resources
