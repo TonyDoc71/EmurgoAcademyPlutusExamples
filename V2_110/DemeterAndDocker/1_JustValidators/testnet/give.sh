092d6f88f807abc4fcf7cc6ed77e53c26a08e8ef
tony_livingston@live.com.au <tony_livingston@live.com.au>
2024-02-29 12:17:01 +0000

    update

--- V2_110/DemeterAndDocker/1_JustValidators/testnet/give.sh
+++ V2_110/DemeterAndDocker/1_JustValidators/testnet/give.sh
@@ -1,9 +1,10 @@
-utxoin="0064391f48e814a5259e905f426e438c65ffba6b0bb750c7a438f58c35370c23#0"
+utxoin="eeafd5c0f3d2c9c010a150a297a099ff973e73eca11a85bbcc9d083821758853#0"
 address=$(cat datum22.addr) 
-output="500000000"
+output="54000000"
 PREVIEW="--testnet-magic 2"
-nami="<provide a wallet to see the tx in blockchain explorers>" 
 
+
+cardano-cli query protocol-parameters --testnet-magic 2 --out-file protocol.params
 
 cardano-cli transaction build \
   --babbage-era \
@@ -11,13 +12,13 @@ cardano-cli transaction build \
   --tx-in $utxoin \
   --tx-out $address+$output \
   --tx-out-datum-hash-file unit.json \
-  --change-address $nami \
+  --change-address "addr_test1qrpzz8whcft2g6z24ft4072s76x8gmj5wn32dxvtr4zcpuqltc67z9ljrqml3hc5mutuher549lwgv2g6n4z0a3ztyesj7sd80" \
   --protocol-params-file protocol.params \
   --out-file give.unsigned
 
 cardano-cli transaction sign \
     --tx-body-file give.unsigned \
-    --signing-key-file ialice.skey \
+    --signing-key-file ../../WalletMine/4payment2.skey \
     $PREVIEW \
     --out-file give.signed
 
--- V2_110/DemeterAndDocker/1_JustValidators/testnet/helper.sh
+++ V2_110/DemeterAndDocker/1_JustValidators/testnet/helper.sh
@@ -1,5 +1,12 @@
 #### Derive address from script
 cardano-cli address build --payment-script-file AlwaysSucceedScript.plutus --testnet-magic 2 --out-file AlwaysSucceedScript.addr
+cardano-cli address build --payment-script-file datum22.plutus --testnet-magic 2 --out-file datum22.addr
 
 #### PubKeyHash creation:
 cardano-cli address key-hash --payment-verification-key-file benef1.vkey --out-file benef1.pkh
+cardano-cli address key-hash --payment-verification-key-file datum22.vkey --out-file datum22.pkh
+
+#### Query UTXo
+datum22.addr
+cardano-cli query utxo --addr_test1wrweuwqm44swpvryhah4y8duf56a5lj4g4v2cx04yau7rqcfgxs6e --testnet-magic 2
+
